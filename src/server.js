f89f02436297f5511a7ccfe3129ba7dac2fb5c4d
leosant <leonardosantos45548@gmail.com>
2020-10-03 02:37:16 -0300

    Criando rotas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const express = require("express")
 const server = express()
 
+//Configurando a pasta pública
+server.use(express.static("public"))
+
+//Configurando os caminhos da minha aplicação
+//Pagina inicial
+//req: Requisição
+//res: Resposta
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+  res.sendFile(__dirname + "/views/create-point.html")
+})
+
 //Ligar Servidor
 server.listen(3000)
